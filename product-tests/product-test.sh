e8f43b181b8399bdf6b6a107b9d8b7d6d77c67d0
akshaym-ecanarys <akshay.mahato@ecanarys.com>
2026-06-17 23:18:00 +0530

    Update product-test.sh

--- product-tests/product-test.sh
+++ product-tests/product-test.sh
@@ -4,12 +4,14 @@ TOTAL=1
 PASSED=0
 FAILED=0
 
+echo "Running Product API Test..."
+
 if curl -f http://localhost:3000/products; then
     PASSED=1
+    echo "Product Test Passed"
 else
     FAILED=1
+    echo "Product Test Failed"
 fi
 
-echo "PRODUCT_TOTAL=$TOTAL" > product-result.txt
-echo "PRODUCT_PASSED=$PASSED" >> product-result.txt
-echo "PRODUCT_FAILED=$FAILED" >> product-result.txt
+echo "$TOTAL,$PASSED,$FAILED" > product-result.txt
