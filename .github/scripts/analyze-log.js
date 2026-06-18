9d2349736ad707ceb90a7f41446736dbc2982f63
akshaym-ecanarys <akshay.mahato@ecanarys.com>
2026-06-18 18:03:25 +0530

    Update analyze-log.js

--- .github/scripts/analyze-log.js
+++ .github/scripts/analyze-log.js
@@ -1,1 +1,10 @@
-console.log("Analyze Log Script Started");
+const fs = require("fs");
+
+console.log("Reading smoke.log...");
+
+const smokeLog = fs.readFileSync("smoke.log", "utf8");
+
+console.log("====================================");
+console.log("SMOKE LOG");
+console.log("====================================");
+console.log(smokeLog);
