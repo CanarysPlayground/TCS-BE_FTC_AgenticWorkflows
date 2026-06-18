feee4f79ef657295e529ee846f2b351ce61f5058
akshaym-ecanarys <akshay.mahato@ecanarys.com>
2026-06-18 14:54:40 +0530

    Update smoke-test.sh

--- smoke-tests/smoke-test.sh
+++ smoke-tests/smoke-test.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+#TOTAL=1
+#PASSED=0
+#FAILED=0
+
+#echo "Running Smoke Test..."
+
+#if curl -f http://localhost:3000/; then
+    #PASSED=1
+    #echo "Smoke Test Passed"
+#else
+    #FAILED=1
+    #echo "Smoke Test Failed"
+#fi
+
+#echo "$TOTAL,$PASSED,$FAILED" > smoke-result.txt
+
+
+#!/bin/bash
+
 TOTAL=1
 PASSED=0
 FAILED=0
 
 echo "Running Smoke Test..."
 
-if curl -f http://localhost:3000/; then
+curl -v http://localhost:3000/login > smoke.log 2>&1
+
+if [ $? -eq 0 ]; then
     PASSED=1
     echo "Smoke Test Passed"
 else
